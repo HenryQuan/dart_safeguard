74a7da441b4b530b4d8623048e203a8561f9b616
Yiheng (Surface) <development.henryquan@gmail.com>
2019-03-28 14:04:17 +1100

    use fold instead of reduce

--- safeguard.dart
+++ safeguard.dart
@@ -10,25 +10,21 @@ void main() {
     }
   };
   
-  var result = Guard(testing, 'More.More.Even.More', null);
+  var result = Guard(testing, 'More.More.Even.123', null);
+  print(result);
+	result = Guard(testing, 'More.More.Even.More', null);
   print(result);
 }
 
 Guard(obj, path, dval) {
-  // This is a just a simple check if obj is null or undefined
+  // check if obj is null
   if (path == '' && obj != null) return obj;
   // check if object is valid and path does not start with or end with '.'
   if (!path.startsWith('.') && !path.endsWith('.')) {
-    // get path as an array and it must have at least 2 elements
-    var p = path.split('.');
-    print(p);
+    List p = path.split('.');
     if (p != null && p.length > 0) {
-      // o is the object (accumulator), and n is from path (current value)
-      // o && o[n] -> to go further or just return default value
-      // only asking for the object
-      var value = p.reduce((o, n) => (o != null && o[n] != null) ? o[n] : dval, obj);
-      print(value);
-      return value;
+      // Take obj as the default value and only go deeper if o and o[n] are both valid
+      return p.fold(obj, (o, n) => (o != null && o[n] != null) ? o[n] : dval);
     }
   } 
   return dval;
